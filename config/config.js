1981989ae219724cdfaa0f6ec955525b0e5e8b2c
Alexandr Gaghen <alexandrgaghen@gmail.com>
2024-05-10 20:21:45 +0300

    1.0

--- config/config.js
+++ config/config.js
@@ -1,6 +1,2 @@
-// export const telegramToken = "Токен вашего бота";
-// export const telegramChatId = "id группы или id контакта";
-
-
-export const telegramToken = "6993934066:AAHcNteK1nMG8wFUdyh059T2CDMWlIZ2_aM";
-export const telegramChatId = "551168869";
+export const telegramToken = "Токен вашего бота";
+export const telegramChatId = "id группы или id контакта";
